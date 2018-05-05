370a1219cdec8e37b3286da4d3df5d6106b6bd48
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-05-05 23:06:38 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
 #write your code here
 
-def countdown
+def countdown(count_down)
+  while count_down > 0
+    puts "#{count_down} SECONDS(S)!"
+    count_down -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
