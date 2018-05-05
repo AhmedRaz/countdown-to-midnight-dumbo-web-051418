49a60a9c60b088446f6d3e2fc869878d02bae442
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-05-05 23:10:37 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -12,7 +12,7 @@ def countdown_with_sleep(count_down)
   while count_down > 0
     puts "#{count_down} SECOND(S)!"
     count_down -= 1
-    sleep(1)
+    sleep(1.second)
   end
   return "HAPPY NEW YEAR!"
 end
